061ead86c3b382f928c088e91479ca5f7adcaab0
José Lucena <jose.lucena@trust.group>
2022-02-02 13:12:29 -0300

    Adicionar modificação no app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. This was modified on feature branch
         </p>
         <p>Docker rules</p>
         <a
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I was changed on the feature branch
+          Learn React
         </a>
       </header>
     </div>
